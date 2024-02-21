3811f5994d8fa31d60ac0be142bc7f990165ab8f
SeelDev <153673121+SeelDev@users.noreply.github.com>
2024-02-21 17:13:56 -0300

    PROYECTO DE TECNICA 6

---
+++ pagina TEC6/js/main.js
@@ -0,0 +1,22 @@
+let resultado = 0 
+let llegada = 200
+let numero = 100
+
+
+/* while( numero <= llegada ) {
+    resultado += 1
+    numero++
+    console.log(resultado)
+} */
+
+
+/* do{
+    console.log('el resultado es ' + resultado);
+    numero--;
+} while (numero <= llegada ) ;
+ */
+
+
+
+
+
