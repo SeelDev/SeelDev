a0d9b6e928ad6749347c0f1ec62e969d2bc34e70
SeelDev <153673121+SeelDev@users.noreply.github.com>
2024-04-17 19:22:17 -0300

    Create clase1.py

---
+++ Python/clase1.py
@@ -0,0 +1,25 @@
+
+#---------------------
+# 		Clase 1
+#---------------------
+''' 
+print('Hola Mundo' , 2+2)
+print(2+2)
+print("2" + "a")#concatenacion
+print(10* "hola")#repeticion
+ 
+ 
+#comentario
+
+#distintas operaciones EXPRESIONES
+print("La suma de 3+4 es : " , 3+4) #suma (esto no se ejecuta)
+print("La resta de 3-4 es : " , 3-4)
+print("La multiplicacion de 3*4 es : " , 3*4)
+print("La division de 9/10 es : " , 9/10)
+print("La modulo de 3//4 es : " , 3//4)
+print("La potencia de 3**4 es : " , 3**4)
+'''
+
+
+
+
